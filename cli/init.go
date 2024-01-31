104e9daf0fe141b0d658604762764b536992cea3
Nihal Sahu <iamnihalsahu@gmail.com>
2024-02-01 05:02:27 +0530

    Create HEAD file on Init

--- cli/init.go
+++ cli/init.go
@@ -9,5 +9,6 @@ func Init() {
 	os.Mkdir(".gcm", os.ModePerm)
 	os.Mkdir(".gcm/snapshots/", os.ModePerm)
 	os.Create(".gcm/gcm.json")
+	os.Create(".gcm/HEAD")
 	fmt.Println("Initialized gcm repository in this directory")
 }
