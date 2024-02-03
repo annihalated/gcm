0b2ac3a73e0047e6172aa3f226b0f316637738af
Nihal Sahu <iamnihalsahu@gmail.com>
2024-02-03 08:54:34 +0530

    Move the init instructions into the function

--- gcm.go
+++ gcm.go
@@ -16,7 +16,11 @@ func Route() {
 	}
 	switch os.Args[1] {
 	case "init":
-		Init()
+		os.Mkdir(".gcm", os.ModePerm)
+		os.Mkdir(".gcm/snapshots/", os.ModePerm)
+		os.Create(".gcm/gcm.json")
+		os.Create(".gcm/HEAD")
+		fmt.Println("Initialized gcm repository in this directory")
 	case "snap":
 		checkForInit()
 
